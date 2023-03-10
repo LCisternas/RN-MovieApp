063fd79033f6da19e07dc83fbc92bc26dea5e30b
LCisternas <l.cisternasopazo@gmail.com>
2023-03-10 18:08:28 -0300

    navigation initial config

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigation } from './src/navigation/Navigation';
 
 const App = () => {
   return (
-    <View></View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 };
 
