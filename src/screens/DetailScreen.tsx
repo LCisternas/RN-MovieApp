27c7b5b128d98a01200cd8fde8a544f88edbe5f9
LCisternas <l.cisternasopazo@gmail.com>
2023-03-13 19:07:59 -0300

    adding icons and new information in detail screen

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,20 +1,60 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions, ScrollView } from 'react-native';
 // import { Movie } from '../interfaces/movieInterface';
 import { RootStackParams } from '../navigation/Navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-interface Props extends StackScreenProps<RootStackParams, 'Detail'> {}
+interface Props extends StackScreenProps<RootStackParams, 'Detail'> { }
+
+const screenHeight = Dimensions.get('screen').height;
 
 const DetailScreen = (props: Props) => {
-  
+
   const movie = props.route.params;
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
 
   return (
-    <View>
-      <Text>DetailScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri }}
+          style={styles.posterImage}
+        />
+      </View>
+      <View style={ styles.marginContainer }>
+        <Text style={ styles.subtitle }>{movie.original_title}</Text>
+        <Text style={ styles.title }>{movie.title}</Text>
+      </View>
+      <View style={ styles.marginContainer }>
+        <Icon name='star-outline' color='grey' size={20} />
+      </View>
+    </ScrollView>
   )
 }
 
 export default DetailScreen
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: '100%',
+    height: screenHeight * 0.7
+  },
+  posterImage: {
+    flex: 1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20
+  },
+  marginContainer: {
+    marginHorizontal: 20,
+    marginTop: 20
+  },
+  title: {
+    fontSize: 21,
+    fontWeight: 'bold'
+  },
+  subtitle: {
+    fontSize: 16,
+    opacity: 0.5
+  }
+})
